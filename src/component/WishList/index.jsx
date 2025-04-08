6539a048c993797ac16dc6738db9caed743ee6d2
SanzinaHossain <sanzina67hossain@gmail.com>
2025-04-08 13:05:51 +0600

    wishlist length problem solve

--- src/component/WishList/index.jsx
+++ src/component/WishList/index.jsx
@@ -3,20 +3,15 @@ import SingleBookList from "./SingleBookList";
 
 export default function WishList() {
   const { wishlist = [], navigate } = WishlistHooks();
+  console.log(wishlist?.length);
   return (
     <div className="p-10">
       <h1 className="text-3xl text-first">Wishlisted Books List:</h1>
-      {wishlist.length > 0 ? (
-        <div className="grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 gap-5 mt-8">
-          {wishlist.map((book) => (
-            <SingleBookList key={book.id} book={book} navigate={navigate} />
-          ))}
-        </div>
-      ) : (
-        <p className="text-red-700 font-bold text-lg mt-5">
-          There is no book found in wish list
-        </p>
-      )}
+      <div className="grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 gap-5 mt-8">
+        {wishlist?.map((book) => (
+          <SingleBookList key={book.id} book={book} navigate={navigate} />
+        ))}
+      </div>
     </div>
   );
 }
